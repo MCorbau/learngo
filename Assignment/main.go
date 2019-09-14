633bc54aff42cbe78212be5fe9f4b2a485789e12
Martin Corbau <martincorbau@hotmail.com>
2019-09-14 19:30:49 +0200

    Learning Assignment

---
+++ Assignment/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Exercise 1
+	color := "green"
+	color = "blue"
+	fmt.Println(color)
+
+	//Exercise 2
+	color = "green"
+	color = "dark " + color
+	fmt.Println(color)
+
+	//Exercise 3
+	n := 0.
+	n = 3.14 * 2
+	fmt.Println(n)
+
+}
